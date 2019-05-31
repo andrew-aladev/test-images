1c5bf52e9c54cb5b43aa7847ce08c5159d90ffeb
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-06-01 00:32:33 +0300

    implementing crossdev for aarch

--- gentoo-stable/aarch64-unknown-linux-gnu/amd64-crossdev/buildah.sh
+++ gentoo-stable/aarch64-unknown-linux-gnu/amd64-crossdev/buildah.sh
@@ -3,7 +3,7 @@ set -e
 
 cd "$(dirname $0)"
 
-container=$(buildah from "$DOCKER_USERNAME/amd64-gentoo-stable-nomultilib:latest")
+container=$(buildah from "docker.io/$DOCKER_USERNAME/amd64-gentoo-stable-nomultilib:latest")
 buildah config --label maintainer="$MAINTAINER" "$container"
 
 copy () {
@@ -27,6 +27,9 @@ build emerge -v sys-devel/crossdev app-emulation/qemu
 build crossdev -t aarch64-unknown-linux-gnu --stable
 
 copy crossdev-root/ /usr/aarch64-unknown-linux-gnu/
+run cd /usr/aarch64-unknown-linux-gnu/usr/bin/ && \
+  cp /usr/bin/qemu-aarch64 qemu-aarch64 && \
+  ln -s qemu-aarch64 qemu-aarch64-static
 
 run rm /usr/aarch64-unknown-linux-gnu/etc/portage/make.profile
 run ln -s /usr/portage/profiles/default/linux/arm64/17.0 /usr/aarch64-unknown-linux-gnu/etc/portage/make.profile
@@ -35,8 +38,6 @@ build aarch64-unknown-linux-gnu-emerge -v1 \
   sys-devel/gcc sys-devel/binutils sys-libs/binutils-libs sys-libs/glibc sys-kernel/linux-headers \
   sys-apps/portage app-shells/bash app-arch/tar sys-devel/make sys-devel/patch \
   sys-apps/findutils sys-apps/grep sys-apps/gawk net-misc/wget
-
-run cp /usr/bin/qemu-aarch64 /usr/aarch64-unknown-linux-gnu/usr/bin/
 
 run rm /usr/aarch64-unknown-linux-gnu/etc/portage/make.profile
 run rm -r /usr/aarch64-unknown-linux-gnu/etc/portage/patches
