50adec1e6d049f3c2630ea81fedf50b08fd2feea
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-08-13 01:00:37 +0300

    implementing aarch64 images

--- aarch64-unknown-linux-musl/amd64-crossdev/buildah.sh
+++ aarch64-unknown-linux-musl/amd64-crossdev/buildah.sh
@@ -3,10 +3,10 @@ set -e
 
 cd "$(dirname $0)"
 
-source "../env.sh"
+source "../../env.sh"
 
-FROM_DOCKER_CONTAINER="${DOCKER_CONTAINER_PREFIX}_amd64-unknown-linux-gnu"
-DOCKER_CONTAINER="${DOCKER_CONTAINER_PREFIX}_aarch64-unknown-linux-gnu_amd64-crossdev"
+FROM_DOCKER_CONTAINER="${DOCKER_CONTAINER_PREFIX}_amd64-pc-linux-gnu"
+DOCKER_CONTAINER="${DOCKER_CONTAINER_PREFIX}_aarch64-unknown-linux-musl_amd64-crossdev"
 
 CPU_COUNT=$(grep -c "^processor" "/proc/cpuinfo")
 MAKEOPTS="-j$CPU_COUNT"
@@ -32,22 +32,22 @@ commit () {
 copy root/ /
 
 build emerge -v sys-devel/crossdev app-emulation/qemu
-build crossdev -t aarch64-unknown-linux-gnu --stable
+build crossdev -t aarch64-unknown-linux-musl --stable
 
-copy crossdev-root/ /usr/aarch64-unknown-linux-gnu/
-run "cd /usr/aarch64-unknown-linux-gnu/usr/bin/ && \
+copy crossdev-root/ /usr/aarch64-unknown-linux-musl/
+run "cd /usr/aarch64-unknown-linux-musl/usr/bin/ && \
   cp /usr/bin/qemu-aarch64 qemu-aarch64 && \
   ln -s qemu-aarch64 qemu-aarch64-static"
 
-run rm /usr/aarch64-unknown-linux-gnu/etc/portage/make.profile
-run ln -s /usr/portage/profiles/default/linux/arm64/17.0 /usr/aarch64-unknown-linux-gnu/etc/portage/make.profile
+run rm /usr/aarch64-unknown-linux-musl/etc/portage/make.profile
+run ln -s /usr/portage/profiles/default/linux/arm64/17.0/musl /usr/aarch64-unknown-linux-musl/etc/portage/make.profile
 
-build aarch64-unknown-linux-gnu-emerge -v1 \
-  sys-devel/gcc sys-devel/binutils sys-libs/glibc sys-kernel/linux-headers \
+build aarch64-unknown-linux-musl-emerge -v1 \
+  sys-devel/gcc sys-devel/binutils sys-libs/musl sys-kernel/linux-headers \
   sys-apps/portage app-shells/bash app-arch/tar sys-devel/make sys-devel/patch \
   sys-apps/findutils sys-apps/grep sys-apps/gawk net-misc/wget
 
-run rm /usr/aarch64-unknown-linux-gnu/etc/portage/make.profile
-run rm -r /usr/aarch64-unknown-linux-gnu/etc/portage/patches
+run rm /usr/aarch64-unknown-linux-musl/etc/portage/make.profile
+run rm -r /usr/aarch64-unknown-linux-musl/etc/portage/patches
 
 commit
