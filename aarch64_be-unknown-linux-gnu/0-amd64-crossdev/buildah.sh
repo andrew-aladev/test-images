7a4cacfcf477020cea07d996043576851898e599
Andrew Aladjev <aladjev.andrew@gmail.com>
2019-09-02 01:51:31 +0300

    removed ncurses from cross compilation

--- aarch64_be-unknown-linux-gnu/0-amd64-crossdev/buildah.sh
+++ aarch64_be-unknown-linux-gnu/0-amd64-crossdev/buildah.sh
@@ -25,7 +25,7 @@ run "cd /usr/aarch64_be-unknown-linux-gnu/usr/bin/ && \
 run rm /usr/aarch64_be-unknown-linux-gnu/etc/portage/make.profile
 run ln -s /usr/portage/profiles/default/linux/arm64/17.0/big-endian /usr/aarch64_be-unknown-linux-gnu/etc/portage/make.profile
 
-build aarch64_be-unknown-linux-gnu-emerge -v1 \
+build USE=\"-ncurses\" aarch64_be-unknown-linux-gnu-emerge -v1 \
   sys-devel/gcc sys-devel/binutils sys-libs/glibc sys-kernel/linux-headers \
   sys-apps/portage app-shells/bash app-arch/tar sys-devel/make sys-devel/patch \
   sys-apps/findutils sys-apps/grep sys-apps/gawk net-misc/wget
