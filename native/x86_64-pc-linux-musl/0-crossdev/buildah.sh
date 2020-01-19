26155338e8aad10f0b8e648b0594d43a2d1e7a0c
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-01-19 14:32:30 +0300

    implementing musl image workarounds

--- native/x86_64-pc-linux-musl/0-crossdev/buildah.sh
+++ native/x86_64-pc-linux-musl/0-crossdev/buildah.sh
@@ -37,8 +37,39 @@ build "${TARGET}-emerge" -v1 \
   app-shells/bash app-arch/tar sys-devel/make sys-devel/patch \
   sys-apps/findutils sys-apps/grep sys-apps/gawk net-misc/wget
 
+# TODO remove this workaround after https://github.com/gentoo/gentoo/pull/9822 will be merged.
+build "${TARGET}-emerge" -v1 dev-lang/python:3.6
+
+run find "/usr/${TARGET}/lib" -maxdepth 1 -name ld* \
+  -exec cp "{}" /lib/ \;
+run sed -i "s/export PYTHON=\${EPREFIX}\/usr\/bin\/\${impl}/export PYTHON=${TARGET}-\${impl}/g" \
+  /usr/portage/eclass/python-utils-r1.eclass
+run sed -i "s/\${EPYTHON:-python}/${TARGET}-\${EPYTHON:-python}/g" \
+  /usr/portage/eclass/distutils-r1.eclass
+run sed -i "s/esetup.py install \(.*\)/esetup.py install \1 --prefix=\"\/usr\"/g" \
+  /usr/portage/eclass/distutils-r1.eclass
+run find /usr/portage/sys-apps/portage -maxdepth 1 -name portage-*.ebuild \
+  -exec sed -i "s/\${D%\/}\${PYTHON_SITEDIR}/\${D%\/}\${PYTHON_SITEDIR#\${EROOT%\/}}/g" "{}" \; \
+  -exec ebuild "{}" manifest \;
+
+copy target-python3.6.sh "/usr/bin/${TARGET}-python3.6"
+run eval " \
+  cd /usr/bin && \
+  ln -s \"${TARGET}-python3.6\" \"${TARGET}-python3\" && \
+  ln -s \"${TARGET}-python3.6\" \"${TARGET}-python\""
+# TODO end of workaround
+
 build "${TARGET}-emerge" -v1 sys-apps/portage
 
-run rm "/usr/${TARGET}/etc/portage/make.profile"
+# TODO remove this workaround after https://github.com/gentoo/gentoo/pull/9822 will be merged.
+run find "/usr/${TARGET}/usr/lib" \( -path "*/python-exec/python3.6/*" -o -path "*/portage/python3.6/*" \) -type f \
+  -exec sed -i "s/#\!\/usr\/${TARGET}\/usr\/bin\/python/#\!\/usr\/bin\/python/g" "{}" \;
+# TODO end of workaround
+
+run eval " \
+  cd \"/usr/${TARGET}/etc/portage\" && \
+  rm make.profile && \
+  rm -r package.keywords && \
+  rm -r patches"
 
 commit
