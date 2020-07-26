19115054ccb16218eb23e13f9b6a122b78db55a7
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-07-26 17:15:19 +0300

    implemented base image for i686, cosmetics

--- native/i686-pc-linux-gnu/env.sh
+++ native/i686-pc-linux-gnu/env.sh
@@ -4,8 +4,7 @@ set -e
 DIR=$(dirname "${BASH_SOURCE[0]}")
 source "${DIR}/../../env.sh"
 
-FROM_DOCKER_USERNAME="gentoo"
+FROM_USERNAME="gentoo"
 FROM_IMAGE_NAME="stage3-x86"
 IMAGE_NAME="${IMAGE_PREFIX}_i686-pc-linux-gnu"
-
-REBUILD_DATE=$(< "${DIR}/.rebuild_date") || :
+IMAGE_PLATFORM="linux/x86"
