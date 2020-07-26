ce706a1785f5f90160d38f066dfdcda3b9a1b251
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-07-26 15:57:58 +0300

    refactoring base image

--- native/x86_64-pc-linux-gnu/env.sh
+++ native/x86_64-pc-linux-gnu/env.sh
@@ -4,8 +4,7 @@ set -e
 DIR=$(dirname "${BASH_SOURCE[0]}")
 source "${DIR}/../../env.sh"
 
-FROM_DOCKER_USERNAME="gentoo"
+FROM_USERNAME="gentoo"
 FROM_IMAGE_NAME="stage3-amd64-nomultilib"
+IMAGE_PLATFORM="linux/amd64"
 IMAGE_NAME="${IMAGE_PREFIX}_x86_64-pc-linux-gnu"
-
-REBUILD_DATE=$(< "${DIR}/.rebuild_date") || :
--- utils.sh
+++ utils.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-CPU_COUNT=$(grep -c "^processor" "/proc/cpuinfo")
-MAKEOPTS="-j${CPU_COUNT}"
-
-# https://github.com/containers/buildah/issues/2165
 XDG_RUNTIME_DIR="/tmp/buildah-user"
 mkdir -p "$XDG_RUNTIME_DIR"
 
@@ -20,10 +16,6 @@ tool () {
 
 # -----
 
-unshare () {
-  tool unshare "$1"
-}
-
 from () {
   tool from "$1"
 }
@@ -36,41 +28,29 @@ unmount () {
   tool unmount "$1"
 }
 
-config () {
-  command=$(quote_args "$@")
-  command="--label maintainer=\"${MAINTAINER}\" ${command} ${CONTAINER}"
-  eval tool config "$command"
-}
-
 copy () {
-  tool copy "$CONTAINER" "$1" "$2"
-}
+  source="$1"
+  destination="$2"
+  container="${3:-${CONTAINER}}"
 
-run () {
-  command=$(quote_args "$@")
-  tool run "$CONTAINER" -- sh -c "$command"
+  tool copy "$container" "$source" "$destination"
 }
 
 build () {
-  command=$(quote_args "$@")
-  command="MAKEOPTS=\"${MAKEOPTS}\" ${command}"
+  from_username="${1:-${FROM_USERNAME}}"
+  from_image_name="${2:-${FROM_IMAGE_NAME}}"
+  maintainer="${3:-${MAINTAINER}}"
+  image_platform="${4:-${IMAGE_PLATFORM}}"
 
-  tool run \
+  tool bud \
+    --build-arg FROM_IMAGE="docker.io/${from_username}/${from_image_name}" \
+    --label maintainer="${maintainer}" \
+    --platform="$image_platform" \
     --cap-add=CAP_SYS_PTRACE \
     --cap-add=CAP_SETFCAP \
-    "$CONTAINER" -- sh -c "$command"
-}
-
-# -----
-
-commit () {
-  image_name="${1:-${IMAGE_NAME}}"
-  docker_username="${2:-${DOCKER_USERNAME}}"
-  container="${3:-${CONTAINER}}"
-  docker_image_name="docker://docker.io/${docker_username}/${image_name}"
-
-  tool commit --format docker "$container" "$image_name"
-  tool tag "$image_name" "$docker_image_name"
+    --isolation="rootless" \
+    --layers \
+    "."
 }
 
 push () {
@@ -93,48 +73,4 @@ pull () {
 
   tool pull "$docker_image_name"
   tool tag "$docker_image_name" "$image_name"
-}
-
-# -----
-
-get_image_created_date () {
-  tool inspect --format "{{.Docker.Created.UTC.Format \"2006-01-02 15:04:05\"}}" "$1"
-}
-
-get_today_date () {
-  date -u +"%Y-%m-%d %H:%M:%S"
-}
-
-check_up_to_date () {
-  from_image_name="${1:-${FROM_IMAGE_NAME}}"
-  image_name="${2:-${IMAGE_NAME}}"
-
-  from_image=$(tool images -q "$from_image_name" || :)
-  image=$(tool images -q "$image_name" || :)
-
-  if [ -z "$from_image" ]; then
-    >&2 echo "from image is required"
-    exit 1
-  fi
-
-  if [ -z "$image" ]; then
-    echo "image: ${IMAGE_NAME} has not yet been built"
-    return
-  fi
-
-  from_image_created_date=$(get_image_created_date "$from_image")
-  image_created_date=$(get_image_created_date "$image")
-
-  if [[ ! "$from_image_created_date" < "$image_created_date" ]]; then
-    echo "from image: ${FROM_IMAGE_NAME} is more recent than image: ${IMAGE_NAME}, it will be rebuilt"
-    return
-  fi
-
-  if ([ ! -z "$REBUILD_DATE" ] && [[ ! "$REBUILD_DATE" < "$image_created_date" ]]); then
-    echo "image: ${IMAGE_NAME} will be rebuilt"
-    return
-  fi
-
-  echo "image: ${IMAGE_NAME} is already up to date"
-  exit 0
 }
