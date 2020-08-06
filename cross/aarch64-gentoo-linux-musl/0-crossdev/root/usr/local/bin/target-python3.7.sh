5ea30874c5e7209619de544939850f7445d4d9a1
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-08-06 23:05:32 +0300

    implementing aarch64 musl image

---
+++ cross/aarch64-gentoo-linux-musl/0-crossdev/root/usr/local/bin/target-python3.7.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+DIR=$(dirname "${BASH_SOURCE[0]}")
+source "${DIR}/target-env.sh"
+
+TARGET_PREFIX="/usr/${TARGET}"
+LIBRARY_PATHES=("lib" "usr/lib" "usr/lib/gcc/${TARGET}/*")
+LD_LIBRARY_PATHES=("${LD_LIBRARY_PATH%:}")
+
+for LIBRARY_PATH in "${LIBRARY_PATHES[@]}"; do
+  LD_LIBRARY_PATHES+=($(ls -d ${TARGET_PREFIX}/${LIBRARY_PATH}))
+done
+
+function join {
+  local IFS="$1"
+  shift
+  echo "$*"
+}
+
+LD_LIBRARY_PATH=$(join ":" "${LD_LIBRARY_PATHES[@]}") \
+  "${TARGET_PREFIX}/usr/bin/python3.7" "$@"
