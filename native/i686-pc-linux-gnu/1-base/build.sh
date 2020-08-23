4bc2f569efc5187a6f282ced2b0bc99db095c0d4
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-08-23 23:33:58 +0300

    implemented i686 pc image using x86 64 crossdev, basic i686 gentoo image is not reliable

---
+++ native/i686-pc-linux-gnu/1-base/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+DIR=$(dirname "${BASH_SOURCE[0]}")
+cd "$DIR"
+
+source "../../../utils.sh"
+source "./env.sh"
+
+container=$(attach "/usr/${TARGET}")
+build || error=$?
+detach "$container" || true
+
+if [ ! -z "$error" ]; then
+  exit "$error"
+fi
--- native/i686-pc-linux-gnu/env.sh
+++ native/i686-pc-linux-gnu/env.sh
@@ -4,6 +4,4 @@ set -e
 DIR=$(dirname "${BASH_SOURCE[0]}")
 source "${DIR}/../../env.sh"
 
-FROM_IMAGE="docker.io/gentoo/stage3-x86"
-IMAGE_NAME="${IMAGE_PREFIX}_i686-pc-linux-gnu"
-IMAGE_PLATFORM="linux/x86"
+TARGET="i686-pc-linux-gnu"
