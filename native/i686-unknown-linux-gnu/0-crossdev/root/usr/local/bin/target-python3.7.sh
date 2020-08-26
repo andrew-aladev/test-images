7275b0c15c41c5ef858ee2dd620c5785001df49b
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-08-26 22:47:04 +0300

    implemented another way of running target python

--- native/i686-unknown-linux-gnu/0-crossdev/root/usr/local/bin/target-python3.7.sh
+++ native/i686-unknown-linux-gnu/0-crossdev/root/usr/local/bin/target-python3.7.sh
@@ -5,14 +5,10 @@ DIR=$(dirname "${BASH_SOURCE[0]}")
 source "${DIR}/target-env.sh"
 
 TARGET_PREFIX="/usr/${TARGET}"
+LOADER_PATH="lib/ld-linux*"
 LIBRARY_PATHES=("lib" "usr/lib" "usr/lib/gcc/${TARGET}/*")
-LD_LIBRARY_PATHES=("${LD_LIBRARY_PATH%:}")
 
-for LIBRARY_PATH in "${LIBRARY_PATHES[@]}"; do
-  LD_LIBRARY_PATHES+=($(ls -d ${TARGET_PREFIX}/${LIBRARY_PATH}))
-done
-
-LD_PATH=$(ls -d ${TARGET_PREFIX}/lib/ld-linux*)
+LD_PATH=$(ls -d ${TARGET_PREFIX}/${LOADER_PATH})
 
 function join {
   local IFS="$1"
@@ -20,5 +16,13 @@ function join {
   echo "$*"
 }
 
-LD_LIBRARY_PATH=$(join ":" "${LD_LIBRARY_PATHES[@]}") "$LD_PATH" \
+LD_LIBRARY_PATHES=("${LD_LIBRARY_PATH%:}")
+
+for LIBRARY_PATH in "${LIBRARY_PATHES[@]}"; do
+  LD_LIBRARY_PATHES+=($(ls -d ${TARGET_PREFIX}/${LIBRARY_PATH}))
+done
+
+LD_LIBRARY_PATH=$(join ":" "${LD_LIBRARY_PATHES[@]}")
+
+"$LD_PATH" --library-path "$LD_LIBRARY_PATH" \
   "${TARGET_PREFIX}/usr/bin/python3.7" "$@"
