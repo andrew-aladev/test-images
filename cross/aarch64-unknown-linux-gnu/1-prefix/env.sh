6ac8c368874cead34ba3022c23135499910af6cc
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-09-12 16:21:52 +0300

    implementing toolchains images

--- cross/aarch64-unknown-linux-gnu/1-prefix/env.sh
+++ cross/aarch64-unknown-linux-gnu/1-prefix/env.sh
@@ -4,6 +4,6 @@ set -e
 DIR=$(dirname "${BASH_SOURCE[0]}")
 source "${DIR}/../env.sh"
 
-FROM_IMAGE="docker.io/gentoo/stage3-amd64-nomultilib"
+FROM_IMAGE="${IMAGE_PREFIX}_${TARGET}_toolchain"
 IMAGE_NAME="${IMAGE_PREFIX}_${TARGET}_prefix"
 IMAGE_PLATFORM="linux/amd64"
