d4ef77cdbbd075f4bb0cf633daeccae5e7316d62
Andrew Aladjev <aladjev.andrew@gmail.com>
2020-09-19 17:13:16 +0300

    fixed target cleanup

--- cross/aarch64-unknown-linux-gnu/0-toolchain/root/usr/local/bin/target-cleanup.sh
+++ cross/aarch64-unknown-linux-gnu/0-toolchain/root/usr/local/bin/target-cleanup.sh
@@ -4,6 +4,5 @@ set -e
 DIR=$(dirname "${BASH_SOURCE[0]}")
 source "${DIR}/target-env.sh"
 
-"${TARGET}-emerge" --depclean
 rm -rf "/usr/${TARGET}/var/tmp/portage/"{*,.*} || :
 rm -rf "/usr/${TARGET}/tmp/portage/"{*,.*} || :
