de3fa35600cab3fcca03a7d5b7cfc1af8a636478
Andrew Aladjev <aladjev.andrew@gmail.com>
2021-05-11 20:58:00 +0300

    scripts cosmetics

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,4 +6,4 @@ cd "$DIR"
 
 git fetch --all || true
 git fetch --tags || true
-git remote | xargs -n1 -I {} git rebase "{}/$(git branch --show-current)" || true
+git remote | xargs -I {} git rebase "{}/$(git branch --show-current)" || true
