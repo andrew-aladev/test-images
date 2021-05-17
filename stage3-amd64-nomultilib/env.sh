d10a0838333a969e79dbab2dc7b77a557f9f6d0c
Andrew Aladjev <aladjev.andrew@gmail.com>
2021-05-18 01:00:29 +0300

    disabled image layers for portage and stage3

--- stage3-amd64-nomultilib/env.sh
+++ stage3-amd64-nomultilib/env.sh
@@ -5,7 +5,8 @@ DIR=$(dirname "${BASH_SOURCE[0]}")
 source "${DIR}/../env.sh"
 
 FROM_IMAGE_PROCESSOR="docker.io/alpine"
-IMAGE_NAME="${IMAGE_PREFIX}_stage3-amd64-nomultilib"
-IMAGE_PLATFORM="linux/amd64"
 AUTOBUILDS_URL="https://mirror.yandex.ru/gentoo-distfiles/releases/amd64/autobuilds"
 SIGN_KEY="0xBB572E0E2D182910"
+IMAGE_NAME="${IMAGE_PREFIX}_stage3-amd64-nomultilib"
+IMAGE_PLATFORM="linux/amd64"
+IMAGE_LAYERS="false"
