aabc47460c87836c6353c489a2bd5218e1e89851
Andrew Aladjev <aladjev.andrew@gmail.com>
2021-05-31 22:21:39 +0300

    using rackspace instead of leaseweb

--- stage3-amd64-nomultilib/env.sh
+++ stage3-amd64-nomultilib/env.sh
@@ -5,7 +5,7 @@ DIR=$(dirname "${BASH_SOURCE[0]}")
 source "${DIR}/../env.sh"
 
 FROM_IMAGE_PROCESSOR="${DOCKER_HOST}/alpine"
-AUTOBUILDS_URL="https://mirror.leaseweb.com/gentoo/releases/amd64/autobuilds"
+AUTOBUILDS_URL="https://mirror.rackspace.com/gentoo/releases/amd64/autobuilds"
 SIGN_KEY="0xBB572E0E2D182910"
 
 IMAGE_BUILD_ARGS="FROM_IMAGE_PROCESSOR AUTOBUILDS_URL SIGN_KEY"
