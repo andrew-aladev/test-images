46393e3184b38509112b17c8b5575322df14a9d0
Andrew Aladjev <aladjev.andrew@gmail.com>
2021-08-05 00:05:05 +0300

    cosmetics

--- i686-unknown-linux-gnu/2-base/env.sh
+++ i686-unknown-linux-gnu/2-base/env.sh
@@ -8,6 +8,5 @@ FROM_IMAGE_PREFIX="${IMAGE_PREFIX}_${TARGET}_prefix"
 
 IMAGE_BUILD_ARGS="FROM_IMAGE_PREFIX TARGET"
 IMAGE_NAME="${IMAGE_PREFIX}_${TARGET}_base"
-# TODO replace with linux/i686 when https://github.com/containers/buildah/issues/3252 will be fixed.
 IMAGE_PLATFORM="linux/amd64"
 IMAGE_LAYERS="false"
