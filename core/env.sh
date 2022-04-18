2a73219848c4f2d11ed25a723503b7a35fc958ed
Andrew Aladjev <aladjev.andrew@gmail.com>
2022-04-19 02:32:33 +0300

    fixed core test image env

--- core/env.sh
+++ core/env.sh
@@ -4,4 +4,4 @@ set -e
 DIR=$(dirname "${BASH_SOURCE[0]}")
 source "${DIR}/../env.sh"
 
-TARGET="i686-unknown-linux-gnu"
+IMAGE_PREFIX="test"
--- env.sh
+++ env.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 MAINTAINER="Andrew Aladjev <aladjev.andrew@gmail.com>"
-IMAGE_PREFIX="test"
 DOCKER_HOST="docker.io"
 DOCKER_USERNAME="puchuu"
