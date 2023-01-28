14b72cd2aece8df7600bdc8b4ced6bac5ade7112
Andrew Aladjev <aladjev.andrew@gmail.com>
2023-01-29 00:12:45 +0300

    fixed from in compression images

--- compression/i386-alpine-linux-musl/env.sh
+++ compression/i386-alpine-linux-musl/env.sh
@@ -5,7 +5,7 @@ DIR=$(dirname "${BASH_SOURCE[0]}")
 source "${DIR}/../env.sh"
 
 TARGET="i386-alpine-linux-musl"
-FROM_IMAGE="${DOCKER_HOST}/${DOCKER_USERNAME}/test_${TARGET}"
+FROM_IMAGE="localhost/test_${TARGET}"
 
 IMAGE_BUILD_ARGS="FROM_IMAGE"
 IMAGE_NAME="${IMAGE_PREFIX}_${TARGET}"
--- compression/x86_64-pc-linux-gnu/env.sh
+++ compression/x86_64-pc-linux-gnu/env.sh
@@ -5,7 +5,7 @@ DIR=$(dirname "${BASH_SOURCE[0]}")
 source "${DIR}/../env.sh"
 
 TARGET="x86_64-pc-linux-gnu"
-FROM_IMAGE="${DOCKER_HOST}/${DOCKER_USERNAME}/test_${TARGET}"
+FROM_IMAGE="localhost/test_${TARGET}"
 
 IMAGE_BUILD_ARGS="FROM_IMAGE"
 IMAGE_NAME="${IMAGE_PREFIX}_${TARGET}"
